0b4ae8b3e5680d9d7e89a04a856c7f1dd82fbd02
aceber <rebecadivinapc@gmail.com>
2024-03-14 12:01:00 -0300

    aula02

---
+++ Aulas_de_conhecimento/aula02.py
@@ -0,0 +1,31 @@
+# Argumentos não nomeados
+print(12,34)
+print(12,34, sep='-')# Para separar os argumentos, pode ser usado aspas duplas.
+print(12,34, sep='-',end='\n')
+
+"""
+    Python = Linguagem de programacao
+    Tipo de tipagem = Dinamica / Forte
+    str -> string -> texto
+    Strings sao textos que estao dentro de aspas
+"""
+print("--String--")
+print("Rebeca")
+print('Rebeca Divina')
+
+print("Rebeca \"Divina\"") #ou
+print('Rebeca "Divina"')
+
+#int
+print("--Int--")
+print(11) 
+print(-11)
+print(0)
+#Float
+print("--Float--")
+print(11.1)
+print("--Bool--")
+print(10 == 11)
+print(10 == 10)
+print("--Funcao type--")
+print(   type('Rebeca')   )
