5daccd222ed17b080741d1028b6f101593ed1752
aceber <rebecadivinapc@gmail.com>
2024-06-06 16:45:57 -0300

    117

---
+++ Aulas_de_conhecimento/aula117.py
@@ -0,0 +1,16 @@
+# Exercícios
+# Crie funções que duplicam, triplicam e quadruplicam
+# o número recebido como parâmetro.
+
+def valorVezQuantidade(valor, quantidade):
+    return valor * quantidade
+def duplica(valor):
+    return valor * 2
+def triplica(valor):
+    return valor * 3
+def quadruplica(valor):
+    return valor * 4
+
+print(duplica(2) , " = " , valorVezQuantidade(2,2))
+print(triplica(2) , " = " , valorVezQuantidade(2,3))
+print(quadruplica(2) , " = " , valorVezQuantidade(2,4))
