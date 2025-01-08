e266d34211a440815629e01c0962756a40d8153a
Rebeca <rebecadivinapc@gmail.com>
2025-01-08 14:20:30 -0300

    128

---
+++ Aulas_de_conhecimento/aula128_classes.py
@@ -0,0 +1,21 @@
+# Métodos de classes + factories
+class Pessoa:
+    ano = 2023
+    def __init__(self,nome,idade):
+        self.nome = nome
+        self.idade = idade
+    @classmethod
+    def metodo_de_classe(cls):
+        print("olooo")
+    @classmethod
+    def criar_com_50_anos(cls,nome):
+        print(nome,50)
+
+p1 = Pessoa('João', 34)
+print(Pessoa.ano)
+Pessoa.metodo_de_classe()
+print(Pessoa.ano)
+Pessoa.criar_com_50_anos('Rebeca')
+print(Pessoa.ano)
+
+        
